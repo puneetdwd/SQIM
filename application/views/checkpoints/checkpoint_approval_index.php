c9e1e183b4c380f01a89308048fb815bf7c04445
komal <komal@crgroup.co.in>
2017-07-06 16:47:15 +0530

    Approval Screen

--- application/views/checkpoints/checkpoint_approval_index.php
+++ application/views/checkpoints/checkpoint_approval_index.php
@@ -26,99 +26,63 @@
     </div>
     <!-- END PAGE HEADER-->
     
-    <div class="row">
-        <!--<div class="col-md-3">
-        
-            <div class="portlet light bordered sampling-dashboard-search-portlet">
-                <div class="portlet-title">
-                    <div class="caption">
-                        <i class="fa fa-reorder"></i> Filters
-                    </div>
-                </div>
+    <div class="row" >
+		<div class="col-md-12">
+				<div class="portlet light bordered sampling-dashboard-search-portlet">				
+					<div class="portlet-title">
+						<div class="caption">
+							<i class="fa fa-reorder"></i> Filters
+						</div>
+					</div>
 
-                <div class="portlet-body form">
-                    <form role="form" class="validate-form" method="post">
-                        <div class="form-body">
-                            <div class="alert alert-danger display-hide">
-                                <button class="close" data-close="alert"></button>
-                                You have some form errors. Please check below.
-                            </div>
+					<div class="portlet-body form">
+						<form role="form" class="validate-form" method="post"  action='<?php echo base_url()."checkpoints/search_checkpoints_by_status" ?>'>
+							<div class="form-body">
+								<div class="alert alert-danger display-hide">
+									<button class="close" data-close="alert"></button>
+									You have some form errors. Please check below.
+								</div>
 
-                            <?php if(isset($error)) { ?>
-                                <div class="alert alert-danger">
-                                    <i class="fa fa-times"></i>
-                                    <?php echo $error; ?>
-                                </div>
-                            <?php } ?>
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <div class="form-group" id="sampling-dashboard-inspection-error">
-                                        <label class="control-label">Select Checkpoint:
-                                            <span class="required"> * </span>
-                                        </label>
-                                                
-                                        <select name="checkpoint_id" class="form-control required select2me"
-                                            data-placeholder="Select Checkpoint" data-error-container="#sampling-dashboard-inspection-error">
-                                            <option value="All">All</option>
-                                            <?php $sel_checkpoint = $this->input->post('checkpoint_id'); ?>
-                                            <?php foreach($checkpoints as $checkpoint) { ?>
-                                                <option value="<?php echo $checkpoint['id']; ?>"
-                                                <?php if($sel_checkpoint == $checkpoint['id']) { ?> selected="selected" <?php } ?>>
-                                                    <?php echo $checkpoint['insp_item']; ?>
-                                                </option>
-                                            <?php } ?>        
-                                        </select>
-                                    </div>
-                                </div>
-                                
-                                <div class="col-md-12">
-                                    <div class="form-group" id="sampling-dashboard-line-error">
-                                        <label class="control-label">Select Part Name:
-                                        <span class="required"> * </span></label>
-                                                
-                                        <select name="part_name" class="required form-control select2me" id="part-selector"
-                                            data-placeholder="Select Part Name" data-error-container="#sampling-dashboard-line-error">
-                                            <option value="All">All</option>
-                                            <?php $sel_part = $this->input->post('part_name'); ?>
-                                            <?php foreach($parts as $part) { ?>
-                                                <option value="<?php echo $part['name']; ?>" <?php if($part['name'] == $sel_part) { ?> selected="selected" <?php } ?>>
-                                                    <?php echo $part['name']; ?>
-                                                </option>
-                                            <?php } ?>        
-                                        </select>
-                                    </div>
-                                </div>
-                                
-                                <div class="col-md-12">
-                                    <div class="form-group" id="sampling-dashboard-line-error">
-                                        <label class="control-label">Select Part Number:
-                                        <span class="required"> * </span></label>
-                                                
-                                        <select name="part_id" class="required form-control select2me" id="part-number-selector"
-                                            data-placeholder="Select Part Number" data-error-container="#sampling-dashboard-line-error">
-                                            <option value="All">All</option>
-                                            <?php $sel_part = $this->input->post('part_id'); ?>
-                                            <?php foreach($part_nums as $part_num) { ?>
-                                                <option value="<?php echo $part_num['id']; ?>" <?php if($part_num['id'] == $sel_part) { ?> selected="selected" <?php } ?>>
-                                                    <?php echo $part_num['code']; ?>
-                                                </option>
-                                            <?php } ?>        
-                                        </select>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                            
-                            
-                        <div class="form-actions">
-                            <button class="button" type="submit">Submit</button>
-                        </div>
+								<?php if(isset($error)) { ?>
+									<div class="alert alert-danger">
+										<i class="fa fa-times"></i>
+										<?php echo $error; ?>
+									</div>
+								<?php } ?>
+								<div class="row">
+									<div class="col-md-3">
+										<div class="form-group" id="sampling-dashboard-inspection-error">
+											<label class="control-label">Select Checkpoint Status:
+												
+											</label>													
+											<select name="checkpoint_status" class="form-control  select2me"
+												data-placeholder="Select Checkpoint" data-error-container="#sampling-dashboard-inspection-error" style='width: 20%;'>
+													<option <?php if($selected_status == 'All') { ?> selected="selected" <?php } ?> value="All">All</option>
+													<option <?php if($selected_status == 'Approved') { ?> selected="selected" <?php } ?> value="Approved">Approved</option>
+													<option <?php if($selected_status == 'Pending') { ?> selected="selected" <?php } ?> value="Pending">Pending</option>
+													<option <?php if($selected_status == 'Declined') { ?> selected="selected" <?php } ?> value="Declined">Declined</option>  
+											</select>
+										
+									</div>
+								</div>
+									<div class="col-md-3">
+										<div class="form-actions" style='padding-top: 24px !important;border-top: 0px solid #e7ecf1;'>
+											<button class="button" type="submit">Submit</button>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>                           
+                        
                     </form>
-                </div>
-            </div>
-        </div>-->
+					
+				</div>
+				
+		</div>
+	</div>
+ </div>
         
-        <div class="col-md-12">
+        <div class="col-md-12" style='margin-top: -250px;'>
             
             <div class="portlet light bordered">
                 <div class="portlet-title">
@@ -133,6 +97,10 @@
                         <a class="button normals btn-circle" href="<?php echo base_url()."sampling/sort_inspections"; ?>">
                             <i class="fa fa-plus"></i> Sort Inspections
                         </a>-->
+						<?php if(!empty($approval_items)) { ?>
+						<a href='<?php echo base_url()."checkpoints/change_checkpoints_status_all/Approved" ?>' class="button" type="submit">Approve All</a>
+						<a href='<?php echo base_url()."checkpoints/change_checkpoints_status_all/Declined" ?>' class="button" type="submit">Decline All</a>
+			<?php } ?>
                     </div>
                 </div>
                 <div class="portlet-body form">
@@ -168,7 +136,11 @@
                                                 <td><?php echo $approval_item['insp_item2']; ?></td>
                                                 <td><?php echo $approval_item['spec']; ?></td>
                                                 <td><?php echo date('jS M, Y', strtotime($approval_item['created'])); ?></td>
-                                                <td><?php echo date('jS M, Y', strtotime($approval_item['modified'])); ?></td>
+                                                <td><?php if(!empty($approval_item['modified']))
+															echo date('jS M, Y', strtotime($approval_item['modified']));
+														else 
+															echo 'Not modified yet';
+													?></td>
                                                 <td><?php if($approval_item['status'] == NULL){ echo "Pending";}else{ echo $approval_item['status'];} ?></td>
                                                 <td nowrap>
                                                     <button type="button" class="btn btn-default btn-xs accordion-toggle" data-toggle="collapse" data-target="#detail-<?php echo $key; ?>">
@@ -190,7 +162,7 @@
                                             <tr>
                                                 <td colspan="7" class="hiddenRow">
                                                     <div class="accordian-body collapse row" id="detail-<?php echo $key; ?>">
-                                                        <div class="col-md-3">
+                                                        <div class="col-md-2">
                                                             <div class="form-group">
                                                                 <label class="control-label col-md-5">LSL:</label>
                                                                 <div class="col-md-7">
@@ -201,7 +173,7 @@
                                                             </div>
                                                         </div>
                                                         <!--/span-->
-                                                        <div class="col-md-3">
+                                                        <div class="col-md-2">
                                                             <div class="form-group">
                                                                 <label class="control-label col-md-5">Target:</label>
                                                                 <div class="col-md-7">
@@ -212,7 +184,7 @@
                                                             </div>
                                                         </div>
 
-                                                        <div class="col-md-3">
+                                                        <div class="col-md-2">
                                                             <div class="form-group">
                                                                 <label class="control-label col-md-5">USL:</label>
                                                                 <div class="col-md-7">
@@ -223,7 +195,7 @@
                                                             </div>
                                                         </div>
 
-                                                        <div class="col-md-3">
+                                                        <div class="col-md-2">
                                                             <div class="form-group">
                                                                 <label class="control-label col-md-5">Unit:</label>
                                                                 <div class="col-md-7">
@@ -233,6 +205,23 @@
                                                                 </div>
                                                             </div>
                                                         </div>
+														
+														<div class="col-md-4">
+                                                            <div class="form-group">
+                                                                <label class="control-label col-md-5">Image:</label>
+                                                                <div class="col-md-7">
+																	<p>
+																	<?php if(!empty($approval_item["images"])){ ?>
+																		<a target='_blank' href='<?php echo base_url()."assets/inspection_guides/".$approval_item["product_name"].'/'.$approval_item["part_number"].'/'.$approval_item["images"]; ?>' >See Image</a>
+																	<?php } else{ ?>
+																		<a target='_blank' href='<?php echo base_url()."assets/inspection_guides/default_guide.jpg" ?>' >See Image</a>
+																	<?php } ?>
+																	</p>
+																
+																</div>
+                                                            </div>
+                                                        </div>
+														
                                                     </div>
                                                 </td>
                                             </tr>
